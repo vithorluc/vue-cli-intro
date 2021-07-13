df617573c2a30e782d5061a0c8e78347f88d012c
vithorluc <vithorvarela.academico@gmail.com>
2021-07-12 22:21:16 -0300

    initial commit

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toogleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }}
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel",
+        phone: "0123 45678 90",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toogleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+  computed: {},
+};
+</script>
